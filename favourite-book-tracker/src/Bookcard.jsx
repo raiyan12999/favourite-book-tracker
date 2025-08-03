ae48d6e2f50eaa5c8c3b5222eda5f1e996be5039
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-08-03 09:55:00 -0700

    added basic function call while clicking button

--- favourite-book-tracker/src/Bookcard.jsx
+++ favourite-book-tracker/src/Bookcard.jsx
@@ -1,8 +1,11 @@
 function Bookcard(props){
+
+    const handleFavourite = () => console.log("Handle Favourtie is clicked");
+
     return(<div className="book">
         <p>Name: {props.name}</p>
         <p>Author: {props.author}</p>
-        <button> Add to favourite </button>
+        <button onClick={handleFavourite}> Add to favourite </button>
     </div>);
 }
 
