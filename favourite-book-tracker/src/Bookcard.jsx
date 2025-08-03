5860e155df77c3369cffdc8a0b7686276a8a212c
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-08-03 10:04:09 -0700

    implemented simple clicking and making true and false logic

--- favourite-book-tracker/src/Bookcard.jsx
+++ favourite-book-tracker/src/Bookcard.jsx
@@ -1,6 +1,17 @@
 function Bookcard(props){
 
-    const handleFavourite = () => console.log("Name of the clicked book is: " + props.name);
+    let clicked = false;
+
+    const handleFavourite = () => {
+        if(clicked){
+            clicked = false;
+        }
+        else{
+            clicked = true;
+        }
+        console.log("Name of the clicked book is: " + props.name);
+        console.log("Value of clicked: " + clicked);
+    };
 
     return(<div className="book">
         <p>Name: {props.name}</p>
