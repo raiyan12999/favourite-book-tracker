b94ca38239f69299085d96cd09065296498b5dc4
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-08-03 09:52:37 -0700

    Added favorite button

--- favourite-book-tracker/src/Bookcard.jsx
+++ favourite-book-tracker/src/Bookcard.jsx
@@ -2,6 +2,7 @@ function Bookcard(props){
     return(<div className="book">
         <p>Name: {props.name}</p>
         <p>Author: {props.author}</p>
+        <button> Add to favourite </button>
     </div>);
 }
 
