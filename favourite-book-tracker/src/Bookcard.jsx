8e5ecc6a8b29c852a81823d42878f03c19a1e7f5
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-08-03 10:12:34 -0700

    implemented favourite logic but css not changing

--- favourite-book-tracker/src/Bookcard.jsx
+++ favourite-book-tracker/src/Bookcard.jsx
@@ -1,19 +1,32 @@
 function Bookcard(props){
 
     let clicked = false;
+    let classname = "book";
 
     const handleFavourite = () => {
         if(clicked){
             clicked = false;
+            classname = "book";
         }
         else{
             clicked = true;
+            classname = "favbook";
         }
         console.log("Name of the clicked book is: " + props.name);
         console.log("Value of clicked: " + clicked);
+        console.log("value of classname: " + classname);
     };
 
-    return(<div className="book">
+    
+
+    // if(clicked){
+    //     classname = "favbook";
+    // }
+    // else{
+    //     classname = "book";
+    // }
+
+    return(<div className={classname}>
         <p>Name: {props.name}</p>
         <p>Author: {props.author}</p>
         <button onClick={handleFavourite}> Add to favourite </button>
