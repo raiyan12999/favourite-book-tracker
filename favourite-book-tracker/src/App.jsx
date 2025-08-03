658b48d507d8439fce32a498c59ec4d91ef968ba
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-08-03 09:34:41 -0700

    created the initial structure and hardcoded card components

---
+++ favourite-book-tracker/src/App.jsx
@@ -0,0 +1,13 @@
+import Bookcard from './Bookcard.jsx';
+
+
+function App(){
+  return (<>
+          <Bookcard/>
+          <Bookcard/>
+          <Bookcard/>
+          <Bookcard/>
+        </>);
+}
+
+export default App;
---
+++ favourite-book-tracker/src/Bookcard.jsx
@@ -0,0 +1,8 @@
+function Bookcard(){
+    return(<div className="book">
+        <p>Name: Harry Potter and the deathly hallows</p>
+        <p>Author: J.K Rowling</p>
+    </div>);
+}
+
+export default Bookcard;
